da2a71dd8ee4b108496cd9e5cd9d6d93c2b35f26
unknown <maker03795@hotmail.com>
2023-04-30 01:39:13 +0700

    update admin :fire:

--- fota.ino
+++ fota.ino
@@ -9,7 +9,7 @@
 #define WIFI_SSID         "5/105_2.4G"
 #define WIFI_PASSWORD     "11357902"
 
-esp32FOTA esp32FOTA("esp32-fota-http", "1.0.0", false, true);
+esp32FOTA esp32FOTA("esp32-fota-http", "2.0.0", false, true);
 
 void setup() {
   SPIFFS.begin(true);
@@ -43,6 +43,6 @@ void loop() {
   if (updatedNeeded) {
     esp32FOTA.execOTA();
   }
-  Serial.println("New version 1.0.0");
+  Serial.println("New version 2.0.0");
   delay(2000);
 }
